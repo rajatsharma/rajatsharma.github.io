e40f0b470f1db4ac65130d1a84eb103703566a1d
Rajat Sharma <rajatsharma0720@gmail.com>
2019-04-02 23:10:35 +0530

    Added Og links

--- src/index.js
+++ src/index.js
@@ -8,6 +8,26 @@ function App({ children }) {
     <>
       <Helmet>
         <title>rajatsharma.dev</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+        <meta
+          name="description"
+          content="Hi, I am Rajat Sharma, Full Stack Web Developer with expertise in React-Redux based SSR Applications, Haskell and Purescript"
+        />
+        <meta property="og:title" content="rajatsharma.dev" />
+        <meta
+          property="og:description"
+          content="Hi, I am Rajat Sharma, Full Stack Web Developer with expertise in React-Redux based SSR Applications, Haskell and Purescript"
+        />
+        <meta
+          property="og:image"
+          content="https://avatars2.githubusercontent.com/u/13231434"
+        />
+        <link rel="canonical" href="https://rajatsharma.dev" />
         <link
           href="https://fonts.googleapis.com/css?family=Inconsolata:400,700"
           rel="stylesheet"
