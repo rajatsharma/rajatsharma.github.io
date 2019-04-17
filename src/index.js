b8551d03dd1875ef79cd8ebb018145e8b3fde699
Rajat Sharma <rajatsharma0720@gmail.com>
2019-04-17 23:21:12 +0530

    Changed GA script

--- src/index.js
+++ src/index.js
@@ -26,15 +26,13 @@ function App({ children }) {
           content="Hi, I am Rajat Sharma, Full Stack Web Developer with expertise in React-Redux based SSR Applications, Haskell and Purescript"
         />
         <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-138499061-1"
-        />
-        <script
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments); }
-                    gtag('js', new Date());
-                    gtag('config', 'UA-138499061-1');`,
+            __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-138499061-1', 'auto');
+ga('send', 'pageview');`,
           }}
         />
         <meta
