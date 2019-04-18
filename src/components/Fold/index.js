0a937af63ff1f45301708b9329bba64b5aac2c5d
Rajat Sharma <rajatsharma0720@gmail.com>
2019-04-18 16:43:43 +0530

    Added Ga and dev server fixes

--- src/components/Fold/index.js
+++ src/components/Fold/index.js
@@ -1,15 +1,6 @@
 import { Flex } from '@elementary/components';
-// import styled from 'styled-components';
 
-// export default styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   background: ${props => (props.dark ? '#0C0F0A' : 'inherit')};
-//   color: ${props => (props.dark ? 'white' : 'black')};
-//   padding-bottom: 40px;
-// `;
-
-export default ({ dark, ...props }) => (
+export default ({ withHeader: _withHeader, black: dark, ...props }) => (
   <Flex
     py={({ theme }) =>
       theme.breakpoints([theme.margins.moby, theme.margins.des])
