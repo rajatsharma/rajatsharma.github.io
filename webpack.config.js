902bc4ac4590779caddb66f102fe30479c671d6c
Rajat Sharma <rajatsharma0720@gmail.com>
2019-04-24 00:30:00 +0530

    Self adding offline plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,19 @@
-module.exports = config => {
-  console.log(config.plugins);
+const OfflinePlugin = require('offline-plugin');
+
+module.exports = (config, options, webpack) => {
+  if (options.target === 'web' && !options.dev) {
+    config.plugins.push(
+      new OfflinePlugin({
+        externals: ['rajatsharma.pdf', '/'],
+        appShell: '/',
+        publicPath: '/',
+      }),
+    );
+  }
+  console.log(options);
+  config.plugins.push(
+    new webpack.DefinePlugin({ __CLIENT__: options.target === 'web' }),
+  );
+
   return config;
 };
