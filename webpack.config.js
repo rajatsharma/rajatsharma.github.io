d9136b002d6a6daad3624ded9bbcf758fedcdcc9
Rajat Sharma <rajatsharma0720@gmail.com>
2019-04-24 14:27:46 +0530

    Fixed paths at config

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,17 @@ module.exports = (config, options, webpack) => {
   if (options.target === 'web' && !options.dev) {
     config.plugins.push(
       new OfflinePlugin({
-        externals: ['rajatsharma.pdf', '/'],
+        externals: [
+          'rajatsharma.pdf',
+          '/',
+          'https://fonts.googleapis.com/css?family=Inconsolata:400,700',
+          'https://fonts.googleapis.com/css?family=Roboto:900',
+        ],
         appShell: '/',
         publicPath: '/',
       }),
     );
   }
-  console.log(options);
   config.plugins.push(
     new webpack.DefinePlugin({ __CLIENT__: options.target === 'web' }),
   );
